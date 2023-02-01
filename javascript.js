892166ef39234fb5cfd339b6e619c87ba6731d2a
emShalaby <medo.dice@gmail.com>
2023-02-01 20:52:59 +0200

    redo how the grid is generated :)

--- javascript.js
+++ javascript.js
@@ -3,38 +3,24 @@ const clear=document.querySelector('#clear');
 const rangeText=document.querySelector('.range-text');
 const size=document.querySelector('#size')
 
-rangeText.innerHTML=size.value+' '+'x'+' '+size.value
-
-size.oninput=()=>rangeText.innerHTML=size.value+' '+'x'+' '+size.value
-
-
-
-for (let i=0; i<256; i++){
-    
-    container.innerHTML+="<div class='box'></div>";
-
-}
-
-const boxes=document.querySelectorAll('.box');
-
-boxes.forEach(function (box){
-    box.addEventListener('mouseover',()=>changeColor(box));
-}
-)
-
-
-function changeColor(elem){
-elem.style.backgroundColor='black';
-}
-
-clear.addEventListener('click',clearBoxes);
-
-function clearBoxes(){
-    boxes.forEach(function(box){
-        box.style.backgroundColor='white';
+function makeGrid(rows,columns){
+    for(let i=0; i<rows;i++){
+        let row=document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        
+        for(let j=0; j<columns; j++){
+            let column=document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
     }
-    )
 }
+makeGrid(16,16);
 
+
+//size slider
+rangeText.innerHTML=size.value+' '+'x'+' '+size.value
+size.oninput=()=>rangeText.innerHTML=size.value+' '+'x'+' '+size.value
 
 
