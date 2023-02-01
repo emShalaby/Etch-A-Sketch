49056d557ac26efbfc5927f681a2ec6d615044cd
emShalaby <medo.dice@gmail.com>
2023-02-01 22:18:48 +0200

    a button to toggle the grid lines and code cleaning up

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@ const container=document.querySelector('.container');
 const clear=document.querySelector('#clear');
 const rangeText=document.querySelector('.range-text');
 const size=document.querySelector('#size')
+const borderChange=document.querySelector('#border-change')
+
+rangeText.innerHTML=size.value+' x '+size.value
+//initial grid
+makeGrid(size.value,size.value);
+
+//--------functions-------
 
 //generates the grid
 function makeGrid(rows,columns){
@@ -17,17 +24,47 @@ function makeGrid(rows,columns){
         }
     }
 }
-//initial grid
-makeGrid(size.value,size.value);
+
 
 //size slider
-rangeText.innerHTML=size.value+' x '+size.value
-size.oninput=sizeChange
 function sizeChange() {
     container.innerHTML=''
     rangeText.innerHTML=size.value+' '+'x'+' '+size.value;
     makeGrid(size.value,size.value);
+    // to check if border is on or off
+    if (borderChange.innerHTML.includes('ON')){
+        createBorder();
+        return;
+    }
+    removeBorder();
+}
+
+//to toggle border
+function removeBorder(){
+    var column=document.querySelectorAll('.column');
+    column.forEach(elem=>elem.style.border='0px');
+}
+
+function createBorder(){
+    var column=document.querySelectorAll('.column');
+    column.forEach(elem=>elem.style.border='1px solid black');
+}
+
+function toggleBorder(b){
+    if (b.innerHTML.includes('OFF')){
+        createBorder();
+        b.innerHTML='Border: ON';
+        return;
+    }
+
+    removeBorder(); 
+    b.innerHTML='Border: OFF' ;
 
 }
+
+//------EVENTS-----
+borderChange.addEventListener('click',()=>toggleBorder(borderChange))
+size.oninput=sizeChange
+
 
 
